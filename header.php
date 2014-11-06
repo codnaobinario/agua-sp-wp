e4d49cfd37e92dc7a6046855faea3a7b1a99ef13
Luiz Sócrate <luiz@socrate.com.br>
2014-11-06 20:34:11 -0200

    initial commit
    Header

---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Aliança pela Água</title>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Twitter Card data -->
+  <meta name="twitter:card" content="summary">
+  <!--meta name="twitter:site" content="@"-->
+  <meta name="twitter:title" content="Aliança pela Água">
+  <meta name="twitter:description" content="Estou assistindo ao vivo o lançamento do Água@SP. Assista comigo nesse link: http://aguasp.socioambiental.org/">
+  <!--meta name="twitter:creator" content="@"-->
+  <meta name="twitter:image" content="http://aguasp.socioambiental.org/images/social.png">
+
+  <!-- Open Graph data -->
+  <meta property="og:title" content="Aliança pela Água">
+  <meta property="og:type" content="site">
+  <meta property="og:url" content="http://aguasp.socioambiental.org/">
+  <meta property="og:image" content="http://aguasp.socioambiental.org/images/social.png">
+  <meta property="og:description" content="<?php bloginfo('description'); ?>">
+  <meta property="og:site_name" content="Aliança pela Água">
+  <meta property="fb:admins" content="">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+
+  <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri();?>/js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+  <![endif]-->
+  <script src="<?php echo get_template_directory_uri();?>/js/vendor/modernizr.js"></script>
+</head>
+<body>
+  <header class="navigation">
+    <div class="navigation-wrapper">
+      <a href="index.html" class="logo">
+        <img src="<?php echo get_template_directory_uri();?>/images/logo-small.svg" alt="Aliança pela Água">
+      </a>
+      <a href="" class="navigation-menu-button" id="js-mobile-menu">MENU</a>
+      <div class="nav">
+        <ul id="navigation-menu">
+          <li class="nav-link"><a href="#cadastre-se">assine</a></li>
+          <li class="nav-link"><a href="#a-crise">crise</a></li>
+          <li class="nav-link"><a href="#agenda">agenda mínima</a></li>
+          <li class="nav-link"><a href="#solucoes">soluções</a></li>
+          <li class="nav-link"><a href="#quem-somos">quem somos</a></li>
+          <li class="nav-link"><a href="#compartilhe">compartilhe</a></li>
+          <li class="nav-link"><a href="#contato">contato</a></li>
+        </ul>
+      </div>
+      <div class="navigation-tools">
+        <a class="icon facebook" href="https://www.facebook.com/aguaemsp"><img src="images/icon-facebook.svg" alt="Facebook"></a>
+        <a class="icon twitter" href="https://www.twitter.com/aguaemsp"><img src="images/icon-twitter.svg" alt="Twitter"></a>
+        <a class="icon gplus" href="https://plus.google.com/117674143282441096563/about"><img src="images/icon-gplus.svg" alt="Google Plus"></a>
+        <a class="icon youtube" href="https://www.youtube.com/user/aguaemsp"><img src="images/icon-youtube.svg" alt="Youtube"></a>
+      </div>
+    </div>
+  </header>
---
+++ js/main.js
@@ -0,0 +1,136 @@
+/* globals jQuery, Modernizr */
+
+(function ($) {
+  'use strict';
+
+  $(function () {
+    var menu = $('#navigation-menu');
+    var menuToggle = $('#js-mobile-menu');
+
+    $(menuToggle).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle(function(){
+        if(menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+      });
+    });
+
+    // underline under the active nav item
+    $('.nav .nav-link').click(function() {
+      $('.nav .nav-link').each(function() {
+        $(this).removeClass('active-nav-item');
+      });
+      $(this).addClass('active-nav-item');
+      $('.nav .more').removeClass('active-nav-item');
+    });
+
+    if (!Modernizr.svg) {
+      $('.navigation .logo img, .hero-logo img').attr('src', function (idx, svgLocation) {
+        return svgLocation.replace(/\.svg$/, '.png');
+      });
+    }
+
+    /**
+     * SHARE FACEBOOK
+     */
+    $('.fb-share').click(function (ev) {
+      ev.preventDefault();
+      FB.ui({
+        method: 'share',
+        href: window.location.href
+      }, function(response){});
+    });
+  });
+
+  $('form#register').on('submit', function (e) {
+    e.preventDefault();
+
+    var name = $('#nameInput').val();
+    var email = $('#emailInput').val();
+
+    salvarUsuario({name: name, email: email});
+  });
+
+  // myDataRef.on('child_added', function(snapshot) {
+  //   var message = snapshot.val();
+  //   displayChatMessage(message.name, message.email);
+  // });
+
+  // function displayChatMessage(name, text) {
+  //   $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  //   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  // };
+})(jQuery);
+
+  var myDataRef = new Firebase('https://popping-heat-3998.firebaseio.com/');
+
+  function salvarUsuario (obj) {
+    myDataRef.push(obj);
+
+    $('.registry').html(
+      '<h2>obrigado por se cadastrar!</h2>'+
+      '<p class="centraliza"><img src="/images/icon-sucesso.svg"/></p>'+
+      '<p class="centraliza">Guardamos seus dados com sucesso, assim que tivermos uma novidade entraremos em contato!</p>'
+    );
+  }
+
+  $('#fb-button').on('click', function(e) {
+    e.preventDefault();
+
+    FB.login(function(response) {
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+      } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+      } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+      }
+    }, {scope: 'public_profile,email'});
+  });
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    FB.api('/me', function(response) {
+      salvarUsuario({name: response.name, email: response.email, facebook:response});
+    });
+  }
+
+function signinCallback(authResult) {
+  if (authResult['access_token']) {
+    // Autorizado com sucesso
+    // Ocultar o botão de login agora que o usuário está autorizado, por exemplo:
+    // console.log(authResult);
+    jQuery.getJSON('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+authResult['access_token'])
+      .done(function(data){
+        // console.log(data);
+        salvarUsuario({name: data.name, email: data.email, google: data});
+      });
+  } else if (authResult['error']) {
+    // Ocorreu um erro.
+    // Possíveis códigos de erro:
+    //  "access_denied" - o usuário negou o acesso a seu aplicativo
+    //   "immediate_failed" - não foi possível fazer o login do usuário automaticamente
+    // console.log('There was an error: ' + authResult['error']);
+  }
+}
+
+
+
+function render() {
+  gapi.signin.render('google-button', {
+    'callback': 'signinCallback',
+    'clientid': "624317565694-vb4f0uers8hktak2a05t5j2slb0bod1u.apps.googleusercontent.com",
+    'cookiepolicy': 'single_host_origin',
+    'scope': 'https://www.googleapis.com/auth/userinfo.email'
+  });
+}
+
+$('.single-item').slick();
+
+
+
+
