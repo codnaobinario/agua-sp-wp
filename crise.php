903d2ac308fb5ee5ccd14d2d17732b9f291b1dff
Luiz Sócrate <luiz@socrate.com.br>
2014-11-07 12:28:46 -0200

    Crise

---
+++ crise.php
@@ -0,0 +1,82 @@
+<section id="a-crise">
+  <div class="container">
+    <h2>A crise</h2>
+    <div class="slider single-item">
+    <div class="texto-destaque">
+      <h3>As principais fontes de São Paulo estão secando</h3>
+    </div>
+    <div>
+      <img src="<?php echo get_template_directory_uri();?>/images/slides/Slide3.PNG" alt="">
+    </div>
+    <div>
+      <img src="<?php echo get_template_directory_uri();?>/images/slides/Slide8.PNG" alt="">
+    </div>
+    <div>
+      <img src="<?php echo get_template_directory_uri();?>/images/slides/Slide9.PNG" alt="">
+    </div>
+    <div class="texto-destaque">
+      <h3>Sem chuva, a situação se agrava. Estamos no volume morto</h3>
+    </div>
+    <div><img src="<?php echo get_template_directory_uri();?>/images/slides/Slide6.PNG" alt=""></div>
+    <div class="texto-destaque">
+      <h3>Os manancias continuam a ser degradados</h3>
+    </div>
+    <div>
+      <iframe width='100%' height='520' frameborder='0' src='http://infoamazonia.cartodb.com/viz/621d23a0-5eaa-11e4-ab03-0e853d047bba/embed_map' allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+    </div>
+    <div  class="texto-destaque">
+      <h3>Menos de 30% da região ainda possui vegetação<br/>
+        50% das APPs não possuem vegetação<br/>
+        Situação mais crítica Represa Jacareí: 9% de APPs com vegetação
+      </h3>
+    </div>
+    <div class="texto-destaque">
+      <h3>A falta de água em São Paulo foi causada por muitos fatores:</h3>
+    </div>
+    <div><img src="<?php echo get_template_directory_uri();?>/images/slides/Slide2.PNG" alt=""></div>
+    <div class="texto-destaque">
+      <h3>Gestão baseada em fontes inesgotáveis<br/>
+        Degradação dos Mananciais <br/>
+        Deficit de chuvas <br/>
+        Falta de transparência e participação
+      </h3>
+    </div>
+    <div class="box-com-espaco">
+      <h4>Quem tem poder de decisão?</h4>
+      <ul class="colunas">
+        <li>
+          <h6>Governo Federal</h6>
+          <p><a href="http://www.mma.gov.br/">MMA</a> e <a href="http://ana.gov.br/">ANA</a> são coordenadores da Plano Nacional de Recursos Hídricos</p>
+        </li>
+        <li>
+          <h6>Governo Estadual</h6>
+          <p>Possue o <a href="http://www.daee.sp.gov.br/">DAEE</a>  para outorga e monitoramento, a <a href="http://www.arsesp.sp.gov.br/SitePages/home.aspx">ARSESP</a> para regular o serviço, recomendar racionamento e garantir acesso/divulgação de informação. além da <a href="http://www.cetesb.sp.gov.br/">CETESB</a>:  monitoramento a qualidade da água e emite licenças ambientais.  O Governo do Estado, principal acionista da Sabesp, possui uma <a href="http://www.saneamento.sp.gov.br/">Secretaria de Saneamento e Recursos Hídricos</a>, que coordena o plano estadual.</p>
+        </li>
+        <li>
+          <h6>Prefeituras</h6>
+          <p>Responsáveis pelo saneamento e concessões de serviço.</p>
+        </li>
+        <li>
+          <h6>Comitês de bacia</h6>
+          <p>Colegiados de gestão de recursos hídricos.</p>
+        </li>
+        <li>
+          <h6>Sociedade civil</h6>
+          <p>Discutir, participar e agir.</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="report-download">
+    <div class="report-box">
+      <img class="box" src="<?php echo get_template_directory_uri();?>/images/report-box-azul.svg">
+      <div class="report-link">
+        <a href="<?php echo get_template_directory_uri();?>/files/Agua_SP_PropCurto_29Out.pdf">
+          <img src="<?php echo get_template_directory_uri();?>/images/report-icon-azul.svg">
+          <h5>Baixar relatório completo</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
 <?php get_template_part('hero'); ?>
 <?php get_template_part('apoiar'); ?>
+<?php get_template_part('crise'); ?>
 <?php get_footer(); ?>
