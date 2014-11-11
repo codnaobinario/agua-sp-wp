c4f1791e8a04b620b8bbfbe98fad664f09b89fb1
Lucas Pirola <lucaspirola@gmail.com>
2014-11-11 12:58:46 -0200

    customizando arquivos para deploy; configurando idioma do admin para pt-br

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'site'
+set :repo_url, 'git@github.com:nucleo-digital/agua-sp-wp.git'
 
 # Branch options
 # Prompts for the branch name (defaults to current branch)
@@ -9,7 +9,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # This could be overridden in a stage config file
 set :branch, :master
 
-set :deploy_to, -> { "/srv/www/#{fetch(:application)}" }
+set :deploy_to, -> { "/home/aguasp/production/#{fetch(:application)}" }
 
 set :log_level, :info
 
