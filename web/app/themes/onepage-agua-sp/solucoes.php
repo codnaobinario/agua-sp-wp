b1825b96229bb24f54b8447561b77a2fc9582473
Lucas Pirola <lucaspirola@gmail.com>
2014-11-17 17:00:24 -0200

    ajustes para layout de solucoes

--- web/app/themes/onepage-agua-sp/solucoes.php
+++ web/app/themes/onepage-agua-sp/solucoes.php
@@ -1,21 +1,21 @@
 <section id="solucoes">
-  <div class="container">
     <h2>Soluções</h2>
     <h5>QUER SOLUCIONAR ESSES PROBLEMAS? ESCOLHA AS MELHORES INICIATIVAS OU PROPONHA A SUA.</h5>
 
-    <ul>
-      <?php
+    <?php
       $solucoes = get_posts(array('category' => 'Soluções'));
       foreach($solucoes as $solucao):
-      ?>
-        <li>
+    ?>
+      <div class="solucao">
+      <div class="info">
+        <div class="thumb-image">
           <?php echo simple_thumb($solucao->ID);?>
-          <div class="content">
-            <h6><?php echo $solucao->post_title;?></h6>
-            <?php echo $solucao->post_content;?>
-          </div>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+        </div>
+        <div class="content">
+          <h6><?php echo $solucao->post_title;?></h6>
+          <!-- <?php echo $solucao->post_content;?> -->
+        </div>
+      </div>
+      </div>
+    <?php endforeach; ?>
 </section>
